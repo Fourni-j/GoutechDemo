40ce350d3f2f1786e1aac731d5b1c28076abe8de
Charles-Adrien Fournier <charladfr@me.com>
2025-02-22 17:35:40 +0100

    Initial Commit

---
+++ Imputabot-ios/Imputabot_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imputabot_iosApp.swift
+//  Imputabot-ios
+//
+//  Created by Charles Fournier on 22/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Imputabot_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
